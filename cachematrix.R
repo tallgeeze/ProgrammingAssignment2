329995616c19801a8ec3cfed5f17f45ff93cd3bc
tallgeeze <57326846+tallgeeze@users.noreply.github.com>
2019-11-23 16:36:23 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##Hi,thank you for auditing my assignment, wish you a nice day.
+## functions discribution:
+In this function 
+R.version
+matrix_buffer <- NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix_inv <- NULL
+  init_matrix <-function()
+  {
+    matrix_buffer <<- x
+    matrix_Inv<<- NULL  
+  }
+  init_matrix()
+  bind_inv <- function(temp_inv){matrix_inv <<- temp_inv}
+  get_matrix <- function() matrix_buffer 
+  get_matrix_inv <- function() matrix_inv
+  list(init_matrix = init_matrix, bind_inv=bind_inv, get_matrix=get_matrix,get_matrix_inv= get_matrix_inv)
 }
 
 
@@ -12,4 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matrix_input <-makeCacheMatrix(x)  
+  matrix_buffer<-matrix_input$get_matrix()
+  matrix_inv <- matrix_input$get_matrix_inv()
+  if(!is.null(matrix_inv)){return(matrix_inv)}
+  
+  matrix_inv <- inv(matrix_buffer)
+  matrix_input$bind_inv(matrix_inv)
+  matrix_inv
 }
