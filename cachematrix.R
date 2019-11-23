04ebb432fb21936b5db3a0c4e8649c52e7639d05
tallgeeze <57326846+tallgeeze@users.noreply.github.com>
2019-11-23 17:23:56 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,13 @@
 ##Hi,thank you for auditing my assignment, wish you a nice day.
 ## functions discribution:
-In this function 
+# In this function, you may give a matrix as follows:
+#   normal_matrix <- matrix(c(1,2,3,4), nrow=2)
+# and you can test it with calling function:
+# cache_matrix <- cacheSolve(normal_matrix), 
+# it will check if Inv of this matrix is calculated
+# if not , it will calculate Inverse of it and return Inverse matrix
+# if yes, it will directly return its Inverse matrix
+
 R.version
 matrix_buffer <- NULL
 makeCacheMatrix <- function(x = matrix()) {
@@ -18,16 +25,32 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  matrix_input <-makeCacheMatrix(x)  
-  matrix_buffer<-matrix_input$get_matrix()
+ ## Return a matrix that is the inverse of 'x'
+  matrix_input <-x 
+  matrix_buffer<<-matrix_input$get_matrix()
   matrix_inv <- matrix_input$get_matrix_inv()
-  if(!is.null(matrix_inv)){return(matrix_inv)}
+  if(!is.null(matrix_inv)){
+    print("aha!") 
+    return(matrix_inv)
+    }
   
   matrix_inv <- inv(matrix_buffer)
   matrix_input$bind_inv(matrix_inv)
   matrix_inv
 }
+cat("
+  Hello!Welcome.
+  you may test with follow commands: 
+  test <- makeCacheMatrix(n) 
+  cacheSolve(test) 
+  you can also visit test$get_matrix_inv() to see the cached inverse matrix \n
+  **if you do cacheSolve(test) again, it will print 'aha!' and the Inverse from Cache **\n 
+  you can try some other matrix to test, remember to name your new matrix as below: 
+    cache_newmatrix <- makeCacheMatrix(your new matrix)
+    then :cacheSolve(cache_newmatrix)")
+
+test <- makeCacheMatrix(n)
+cacheSolve(test)
+test$get_matrix_inv()
